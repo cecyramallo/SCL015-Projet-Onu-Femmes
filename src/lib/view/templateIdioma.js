2347deaabc8b750d387f7f588ea6686a9d43ae86
cecyramallo <cecyramallo@gmail.com>
2021-02-17 23:40:04 -0400

    English final version

--- src/lib/view/templateIdioma.js
+++ src/lib/view/templateIdioma.js
@@ -3,12 +3,12 @@
 export const language = () => {
     const divLanguage = document.createElement("div");
     const viewLanguage = `
-        <p>Paramètres linguistiques</p>
-        <p>Choisissez votre langue préférée.</p>
-        <p>Vous pourrez changer la langue après.</p>
-        <p><input type="radio">Français</p>
-        <p><input type="radio">Español</p>
-        <p><input type="radio">English</p>
+        <p id="language-settings">Paramètres linguistiques</p>
+        <p id="choose-language">Choisissez votre langue préférée.</p>
+        <p id="choose-language-again">Vous pourrez la changer après.</p>
+        <p class="input-language"><input type="radio">Français</p>
+        <p class="input-language"><input type="radio">Español</p>
+        <p class="input-language"><input type="radio">English</p>
         <button id="language-button">Continuer</button>
     `;
     divLanguage.innerHTML = viewLanguage;
